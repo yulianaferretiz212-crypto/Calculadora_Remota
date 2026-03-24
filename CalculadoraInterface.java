8af71c35bef73b9f92d4955ce26d9188727d1989
yulianaferretiz212-crypto <yulianaferretiz212@gmail.com>
2026-03-24 12:02:40 -0600

    metodo restar

--- CalculadoraInterface.java
+++ CalculadoraInterface.java
@@ -9,4 +9,6 @@ public interface CalculadoraInterface extends Remote {
     // la llamada vieja por red y puede fallar en cualquier momen to.
 
     double sumar(double a, double b) throws RemoteException;
+
+    double restar(double a, double b) throws RemoteException;
 }
