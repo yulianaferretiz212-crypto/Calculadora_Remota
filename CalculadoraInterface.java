14358a8042853e8af1b2ff0b69da9058173c0ac8
yulianaferretiz212-crypto <yulianaferretiz212@gmail.com>
2026-03-24 12:00:22 -0600

    metodo de suma

---
+++ CalculadoraInterface.java
@@ -0,0 +1,12 @@
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+//Extiende remote: le dice a Java 
+//que esta interfaz puede ser usada desde otra maquina por la red.
+
+public interface CalculadoraInterface extends Remote {
+    // cada metodo lanza RemoteException porque
+    // la llamada vieja por red y puede fallar en cualquier momen to.
+
+    double sumar(double a, double b) throws RemoteException;
+}
