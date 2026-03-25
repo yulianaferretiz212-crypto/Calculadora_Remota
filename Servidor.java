6f66a6420c310303fc3fecca0062b068decd7ef3
yulianaferretiz212-crypto <yulianaferretiz212@gmail.com>
2026-03-25 11:53:05 -0600

    creacion del servidor

--- Servidor.java
+++ Servidor.java
@@ -1,3 +1,10 @@
+import java.rmi.registry.LocateRegistry;
+import java.rmi..registry.registry;
+
+//SERVIDOR
+//Arranca el registro RMI y publica la calculadora
+//Para que los clientes pueden encontrarla y usarla
+
 public class Servidor {
 
 }
