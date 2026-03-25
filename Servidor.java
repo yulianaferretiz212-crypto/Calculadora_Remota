0219d5a66afdce831f5a7e6b06ceedf66083c1c4
yulianaferretiz212-crypto <yulianaferretiz212@gmail.com>
2026-03-25 11:56:22 -0600

    objeto de la logica de la calculadora

--- Servidor.java
+++ Servidor.java
@@ -1,10 +1,20 @@
-import java.rmi.registry.LocateRegistry;
-import java.rmi..registry.registry;
+import java.rmi.registry.LocateRegistry;import java.rmi..registry.registry;
 
 //SERVIDOR
 //Arranca el registro RMI y publica la calculadora
 //Para que los clientes pueden encontrarla y usarla
 
 public class Servidor {
+
+    public static void main(String[] args) {
+        try{
+
+        //Paso 1: Crear el objeto con la logica de la calculadora.
+        //Al instanciar CalculadoraImpl, UnicasteRemoteObject
+        // lo deja listo para recibir llamada por red
+        CalculadoraImpl calculadora = new CalculadoraImpl();
+        
+        }
+    }
 
 }
