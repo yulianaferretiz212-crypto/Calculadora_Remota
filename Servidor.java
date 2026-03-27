98831098514f4ebd37f056b9cf2c4c81f9de3289
yulianaferretiz212-crypto <yulianaferretiz212@gmail.com>
2026-03-27 11:49:08 -0600

    publicar calculadora

--- Servidor.java
+++ Servidor.java
@@ -1,4 +1,6 @@
-import java.rmi.registry.LocateRegistry;import java.rmi..registry.registry;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi..registry.registry;
 
 //SERVIDOR
 //Arranca el registro RMI y publica la calculadora
@@ -14,7 +16,25 @@ public class Servidor {
         // lo deja listo para recibir llamada por red
         CalculadoraImpl calculadora = new CalculadoraImpl();
         
+        //Paso 2: Crear el registro de RMI en el puerto estandar 1099
+        //El Registry es el directorio donde se publican los objetos remotos.
+        Registry registry = LocateRegistry.createRegistry(;1099);
+
+        //Paso 3: Publicar la calculadora con un nombre clave.
+        // El cliente usara este mismo nombre 
+        registry.rebind("Calculadora", calculadora);
+
+        System.out.println("==============================");
+        System.out.println("  Servidor RMI iniciado");
+        System.out.println("  Puerto : 1099");
+        System.out.println("  Servicio: Calculadora");
+        System.out.println("  Esperando clientes...");
+        System.out.println("==============================");
+
+    } catch (Exception e) {
+            System.err.println("Error al iniciar el servidor: " + e.getMessage());
+            e.printStackTrace();
         }
     }
-
 }
+
